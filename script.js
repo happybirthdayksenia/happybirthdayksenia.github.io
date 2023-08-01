826b508329ac1d20b7e55263836a5a2038fc588b
happybirthdayksenia <141158960+happybirthdayksenia@users.noreply.github.com>
2023-08-01 16:45:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,26 @@ function updateLanguage(language) {
   document.getElementById('celebrationsMessage').innerText = languages[language].celebrations;
   document.getElementById('note').innerText = languages[language].note;
 }
+
+const birthdayDate = new Date('2023-08-02T00:00:00+07:00');
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeLeft = birthdayDate - now;
+
+  if (timeLeft > 0) {
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML =
+      hours + 'h ' + minutes + 'm ' + seconds + 's';
+    document.getElementById('countdown').style.display = 'block';
+    document.getElementById('celebrationsMessage').style.display = 'none';
+  } else {
+    document.getElementById('countdown').style.display = 'none';
+    document.getElementById('celebrationsMessage').style.display = 'block';
+  }
+}
+
+setInterval(updateCountdown, 1000);
